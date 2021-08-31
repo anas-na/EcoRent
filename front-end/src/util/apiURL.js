578c95be2754a3305759b0388c23bfa89fd5dace
Kobe <tahiriadam@gmail.com>
2021-08-30 21:41:24 -0400

    update apiURL

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://git.heroku.com/still-forest-94069.git";
+    : "https://still-forest-94069.herokuapp.com/";
 };
