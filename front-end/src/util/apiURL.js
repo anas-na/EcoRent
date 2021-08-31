d350d550d3151d063216791c40ea0d97faef4c4b
Kobe <tahiriadam@gmail.com>
2021-08-30 21:55:31 -0400

    fixed apiURL

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://still-forest-94069.herokuapp.com/";
+    : "https://still-forest-94069.herokuapp.com";
 };
