16a775f3fcef4fedb8748bef80aa0a398a4174aa
Kobe <tahiriadam@gmail.com>
2021-09-11 10:59:15 -0400

    created seed and schema files

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,43 @@
-DROP TABLE IF EXISTS test;
+\c eco_rent_dev;
 
-CREATE TABLE test (
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
     id SERIAL PRIMARY KEY, 
     name TEXT
 );
+
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY, 
+    category_id INT,
+    name TEXT,
+    description TEXT,
+    price INT,
+    location TEXT,
+);
+
+DROP TABLE IF EXISTS transactions;
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY, 
+    item_id INT,
+    owner_id INT,
+    rentee_id INT
+);
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY, 
+    name TEXT,
+    review TEXT,
+    prod_rating INT,
+    owner_rating INT,
+    transaction_id INT
+);
+
+
+
+
