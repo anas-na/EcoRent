2eec65954224b3181d3516fc4f4946af53b48403
Kobe <tahiriadam@gmail.com>
2021-09-14 21:12:03 -0400

    create config file and authroute file

---
+++ front-end/src/components/AuthRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+// import useUser from "hooks/useUser"
+
+const AuthRoute = ({ component=AuthorizeComponent, ...allProps }) => {
+    const { user } = useUser()
+  return (
+    <Route
+      {...allProps}
+      render={(routeProps) => user 
+        ? (<AuthorizeComponent { ...routeProps } />) 
+        : (<Redirect to={"/login"} />)
+    }
+    />
+  );
+};
+
+export default AuthRoute;
