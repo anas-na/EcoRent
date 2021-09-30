6f1302d608450c21439c8b505afcac798bb73b08
Adam Tahiri <75190352+AdamTahiri@users.noreply.github.com>
2021-09-29 20:35:32 -0400

    Google maps (#26)
    * uid progress
    
    * add navbar to app
    
    * no changes to backend
    
    * create itemDetail page
    
    * add googleMaps implementation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,14 +10,19 @@ import MyItems from "./components/MyItems.js";
 import HowItWorks from "./pages/HowItWorks.js";
 import ListAnItem from "./pages/New.js";
 import NavBar from "./components/NavBar";
-import ItemsList from "./components/ItemsList"
 import Index from "./pages/Index.js";
+import ItemDetails from './components/ItemDetails';
+import ItemNew from './components/ItemNew';
 import Profile from "./components/Profile.js";
 
 function App() {
 
   return (
     <div className='App'>
+      <script 
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCboyUY2hGDoFXzjzQ2W78KYbnHv6vMKT4" 
+      async
+    ></script>
       <UserProvider>
       <Router>
       <NavBar />
@@ -25,10 +30,10 @@ function App() {
         <AuthRoute exact path="/" component={Home} />
         <Route exact path="/signup" component={SignUp}/>
         <Route exact path="/login" component={LogIn}/>
-        <Route path ='/howItWorks' component={HowItWorks}/>
-        
+        <AuthRoute path ='/howItWorks' component={HowItWorks}/>
+        <AuthRoute path='/items/:id' component={ItemDetails} />
         <AuthRoute path='/myItems' component={MyItems} />
-        <AuthRoute path ='/items/new' component={ListAnItem}/>
+        <AuthRoute path ='/items/new' component={ItemNew}/>
         <Route path='/items'  component={Index}/>
         <Route path='/myprofile' component={Profile}/>
         {/* <Route path='/account' component={Account}/> */}
---
+++ front-end/src/components/ItemDetails.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { apiURL } from "../util/apiURL";
+import GoogleMap from  "../util/GoogleMap";
+const API = apiURL();
+
+const ItemDetails = () => {
+  const [item, setItem] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getItem = async () => {
+      try {
+        const res = await axios.get(`${API}/items/${id}`);
+        console.log(res);
+        setItem(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItem();
+  }, [id]);
+
+  console.log(item)
+
+  return (
+    <div>
+      {/* <article>
+        <div>
+          <p>Category:</p>
+          <p>Name: {item.name}</p>
+          <p>Description: {item.description}</p>
+          <p>Price: ${item.price}</p>
+          <p>Location: {item.location}</p>
+        </div>
+      </article> */}
+      <GoogleMap />
+    </div>
+  );
+};
+
+export default ItemDetails;
