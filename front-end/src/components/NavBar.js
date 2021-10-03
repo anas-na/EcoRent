150fe860531f76cb1e8fbd8320130c90df1b7b85
Cari <t.carina@rocketmail.com>
2021-10-03 16:34:37 -0400

    styling dropdown

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
   const user = useContext(UserContext);
   const { logOut } = useUser();
   const [open, setOpen] = useState(false);
-  
+
   const handleLogOut = () => {
     try {
       logOut();
@@ -28,47 +28,120 @@ const NavBar = () => {
       <nav>
         <div className="navContainer">
           <div>
-            <Link to="/"><img src={logo} className="logo" /></Link>
+            <Link to="/">
+              <img src={logo} className="logo" />
+            </Link>
           </div>
           <div className="links">
-            <NavLink to="/items" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}}>Items</NavLink>
-            <NavLink to="/myItems" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}}>My Items</NavLink>
-            <NavLink to="/newitem" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}}>List An Item</NavLink>
-            <NavLink to="/account" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}}>Account</NavLink>
+            <NavLink
+              to="/items"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              Items
+            </NavLink>
+            <NavLink
+              to="/myItems"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              My Items
+            </NavLink>
+            <NavLink
+              to="/newitem"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              List An Item
+            </NavLink>
+            <NavLink
+              to="/account"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              Account
+            </NavLink>
           </div>
-          <img
-            src={logo}
-            alt="user"
-            className="profile"
-            onClick={handleDropdown}
-          />
-          <ul className={!open ? "dropdown" : null} id="user-menu">
-            <li className="one">
-              <i className="fas fa-user-circle"></i>
-              <NavLink exact to="/myprofile">
-                Profile
-              </NavLink>
-            </li>
-            <li>
-              <i className="fas fa-sign-out-alt"></i>
 
-              <button onClick={handleLogOut}>Sign Out</button>
-            </li>
-          </ul>
+
+          <section className="dropdownMenu">
+            <img
+              src={logo}
+              alt="user"
+              className="profile"
+              onClick={handleDropdown}
+            />
+            <ul className={!open ? "dropdown" : null} id="user-menu">
+
+              <button className="dropdownButton">
+                <NavLink exact to="/myprofile">
+                  Profile
+                </NavLink>
+              </button>
+
+              <button onClick={handleLogOut} className="dropdownButton">
+                Sign Out
+              </button>
+
+            </ul>
+          </section>
+
+
         </div>
       </nav>
     );
   } else {
     return (
       <nav>
-          <div className='navContainer'>
-        <div>
-          <Link to="/"><img src={logo} className="logo" /></Link>
-        </div>
+        <div className="navContainer">
+          <div>
+            <Link to="/">
+              <img src={logo} className="logo" />
+            </Link>
+          </div>
           <div className="links">
-            <NavLink to="/howItWorks" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}} >How It Works</NavLink>
-            <NavLink to="/about" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}} >About</NavLink>
-            <NavLink to="/login" activeStyle={{color: '#98bd89', fontSize: '1.8em', fontWeight:' bold'}} >SignUp/LogIn</NavLink>
+            <NavLink
+              to="/howItWorks"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              How It Works
+            </NavLink>
+            <NavLink
+              to="/about"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/login"
+              activeStyle={{
+                color: "#98bd89",
+                fontSize: "1.8em",
+                fontWeight: " bold",
+              }}
+            >
+              SignUp/LogIn
+            </NavLink>
           </div>
         </div>
       </nav>
